1a0c4cf0e4b5cb57392a0f72051e677b62a8fe67
David Pineda <davidpinedazuluaga@gmail.com>
2024-10-09 14:51:38 -0500

    Se hace el llamado a la peticion de giphy para traer los gifs y se muestran en pantalla solo los titulos

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +9,45 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'IU2346pBZ4lWQDRdm85OyetK3J32cxfp';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  public gifList: Gif[] = [];
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+  searchTag(tag: string): void {
+
+    if(tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+
+        this.gifList = resp.data;
+
+      });
+
   }
 }
